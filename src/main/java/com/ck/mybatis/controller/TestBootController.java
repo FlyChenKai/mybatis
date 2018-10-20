7365b968dcf986b5928bd49ece4599aaea7f97a7
FlyChenKai <1628343148@qq.com>
2018-10-20 10:01:08 +0800

    mybatis和spring的整合

---
+++ src/main/java/com/ck/mybatis/controller/TestBootController.java
@@ -0,0 +1,20 @@
+package com.ck.mybatis.controller;
+
+import com.ck.mybatis.entity.User;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+@RequestMapping("/testboot")
+public class TestBootController {
+    @RequestMapping("getuser")
+    public User getUser(){
+        User user=new User();
+        user.setName("test");
+        return user;
+    }
+
+
+}
